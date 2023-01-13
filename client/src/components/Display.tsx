29c9430fa9ab81699906b35f39ddbfa18f827398
dvkr <v3dvkr@gmail.com>
2023-01-13 15:54:25 -0700

    minor layout alignment fix

--- client/src/components/Display.tsx
+++ client/src/components/Display.tsx
@@ -4,13 +4,15 @@ export function Display() {
 	const { isRunning, minutes, seconds, increaseMinutes, decreaseMinutes } =
 		useTimerStore((state) => state);
 	return (
-		<div className="flex flex-col md:grid md:grid-cols-5">
+		<div className="flex flex-col md:grid md:grid-cols-5 gap-3">
 			<p className="md:col-span-4 text-center text-6xl sm:text-[10rem]">
 				{minutes.toString().padStart(2, "0")}:
 				{seconds.toString().padStart(2, "0")}
 			</p>
-			<div className="flex md:flex-col md:col-start-5 justify-around pt-8 md:pt-0">
-				<button disabled={isRunning && minutes !== 0 && seconds !== 0}
+			<div className="flex justify-around gap-6 md:flex-col md:col-start-5  pt-8 md:pt-0">
+				<button
+					className="btn sm:h-16 flex-grow"
+					disabled={isRunning && minutes !== 0 && seconds !== 0}
 					onClick={(e) => {
 						e.preventDefault();
 						increaseMinutes();
@@ -18,7 +20,9 @@ export function Display() {
 				>
 					Up
 				</button>
-				<button disabled={isRunning && minutes !== 0 && seconds !== 0}
+				<button
+					className="btn sm:h-16 flex-grow"
+					disabled={isRunning && minutes !== 0 && seconds !== 0}
 					onClick={(e) => {
 						e.preventDefault();
 						decreaseMinutes();
