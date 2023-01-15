740135b99626194d0a89ab7823540ec1ccdb5002
dvkr <v3dvkr@gmail.com>
2023-01-15 13:20:30 -0700

    added settings to limit queries

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,13 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+		  refetchOnWindowFocus: false
+		},
+	  },
+});
 export const loginPath = "app";
 const router = createBrowserRouter([
 	{
