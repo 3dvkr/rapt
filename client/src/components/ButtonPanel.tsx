b14711f7c4ccfdbb5838c2929b30152bd27f5aa2
dvkr <v3dvkr@gmail.com>
2023-01-23 12:22:25 -0700

    fixed category layout

--- client/src/components/ButtonPanel.tsx
+++ client/src/components/ButtonPanel.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import shallow from 'zustand/shallow'
+import shallow from "zustand/shallow";
 import { useTimerStore } from "../store";
 
 export function ButtonPanel({
@@ -10,13 +10,16 @@ export function ButtonPanel({
 	setCategory: React.Dispatch<React.SetStateAction<string>>;
 }) {
 	const { isRunning, hasStarted, setSessionInfo, toggleIsRunning, reset } =
-		useTimerStore((state) => ({
-			isRunning: state.isRunning,
-			hasStarted: state.hasStarted,
-			setSessionInfo: state.setSessionInfo,
-			toggleIsRunning: state.toggleIsRunning,
-			reset: state.reset,
-		}), shallow);
+		useTimerStore(
+			(state) => ({
+				isRunning: state.isRunning,
+				hasStarted: state.hasStarted,
+				setSessionInfo: state.setSessionInfo,
+				toggleIsRunning: state.toggleIsRunning,
+				reset: state.reset,
+			}),
+			shallow
+		);
 
 	const fakeCategories = ["work", "reading", "coding", "break"];
 	useEffect(() => {
@@ -24,23 +27,23 @@ export function ButtonPanel({
 	}, []);
 
 	return (
-		<div className="flex justify-between gap-3 sm:grid sm:grid-cols-5">
-			{fakeCategories.map((c, i) => {
-				return (
-					<button
-						key={i}
-						className={`flex gap-2 align-center btn ${
-							category === c ? "btn-accent" : "btn-ghost"
-						}`}
-						onClick={() => {
-							setCategory(c);
-						}}
-					>
-						{c}
-					</button>
-				);
-			})}
-			<div className={`${isRunning && "btn-group"} sm:col-start-5`}>
+		<div className="gap-3 grid grid-cols-3 sm:grid-cols-5">
+			<div className="col-span-2 sm:col-span-4 grid gap-x-6 gap-y-2 grid-cols-2 sm:grid-cols-4">
+				{fakeCategories.map((c, i) => {
+					return (
+						<button
+							key={i}
+							className={`btn ${category === c ? "btn-accent" : "btn-ghost"}`}
+							onClick={() => {
+								setCategory(c);
+							}}
+						>
+							{c}
+						</button>
+					);
+				})}
+			</div>
+			<div className={`${isRunning && "btn-group"} col-start-3 sm:col-start-5 row-start-1`}>
 				<button
 					className={`btn ${isRunning ? "btn-warning" : "btn-success w-full"}`}
 					onClick={() => {
